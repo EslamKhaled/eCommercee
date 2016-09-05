9349ec61e7c4ccdb7211e0e04151850abdef2581
EslamKhaledTawfik <eslamkhaledtawfik@hotmail.com>
2016-09-05 03:26:28 +0200

    Revert "Up to date project"
    This reverts commit a656b3a49fb760b6bc1b5d8676542514fbd73d97.

--- admin/includes/templates/footer.php
+++
@@ -1,8 +0,0 @@
-        <div class="footer">
-           
-        </div>
-        <script src="<?php echo $js; ?>jquery-2.0.0.min.js"></script>
-        <script src="<?php echo $js; ?>bootstrap.min.js"></script>
-        <script src="<?php echo $js; ?>plugin.js"></script>
-    </body>
-</html>
